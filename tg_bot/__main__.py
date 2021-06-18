b2840c72d3620b7888c0d277612be71fb49014e9
Wade Wilson <84837136+Sayandhridolickid@users.noreply.github.com>
2021-06-18 19:08:08 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -143,7 +143,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
 
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username)),  
+                    [[InlineKeyboardButton(text="🎉 Add me to your group", url="t.me/{}?startgroup=true".format(bot.username))],  
                      [InlineKeyboardButton(text="👥 Support Group", url="https://t.me/Movievibezz"), InlineKeyboardButton(text="🔔 Update Channel", url="https://t.me/Movievibing")],
                      [InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
 
